fdd06800b8a5b9d6d7d10d9c0e2ae83f5e7292b8
JunHyeong Park <suldangoo@gmail.com>
2023-11-11 02:59:29 +0900

    데이터매니저 최신화

--- Assets/Scripts/SaveData/SaveManager.cs
+++ Assets/Scripts/SaveData/SaveManager.cs
@@ -1,112 +1,158 @@
 using UnityEngine;
 
-// PlayerPrefs를 통한 정보 저장 & 불러오기
 public static class SaveManager
 {
-    static string isSaveKey = "isSave";
+    static string progressKey = "progress"; // 진행 여부
     static string genderKey = "gender";
     static string nickNameKey = "nickName";
-    static string bestScoreKey = "bestScore";
+    static string levelKey = "level";
+    static string experienceKey = "experience";
+    static string hpKey = "hp";
+    static string strengthKey = "strength";
+    static string agilityKey = "agility";
+    static string healthKey = "health";
+    static string wisdomKey = "wisdom";
+    static string charmKey = "charm";
+    static string goldKey = "gold";
+    static string weaponKey = "weapon";
+    static string armorKey = "armor";
+    static string smallHpPotionKey = "smallHpPotion";
+    static string largeHpPotionKey = "largeHpPotion";
+
     static string sfxVolumeKey = "sfxVolume";
     static string bgmVolumeKey = "bgmVolume";
 
-    // 진행중인 데이터가 존재하는가
-    public static bool IsSave
+    public static bool Progress
     {
-        get
-        {
-            int i = PlayerPrefs.GetInt(isSaveKey);
-            return i == 1 ? true : false;
-        }
-        set
-        {
-            PlayerPrefs.SetInt(isSaveKey, value ? 1 : 0);
-        }
+        get { return PlayerPrefs.GetInt(progressKey) == 1; }
+        set { PlayerPrefs.SetInt(progressKey, value ? 1 : 0); }
     }
 
-    // 캐릭터 성별
-    // true : 남성 / false : 여성
     public static bool Gender
     {
-        get
-        {
-            int i = PlayerPrefs.GetInt(genderKey);
-            return i == 1 ? true : false;
-        }
-        set
-        {
-            PlayerPrefs.SetInt(genderKey, value ? 1 : 0);
-        }
+        get { return PlayerPrefs.GetInt(genderKey) == 1; }
+        set { PlayerPrefs.SetInt(genderKey, value ? 1 : 0); }
     }
 
-    // 캐릭터 닉네임
     public static string NickName
     {
-        get
-        {
-            return PlayerPrefs.GetString(nickNameKey);
-        }
-        set
-        {
-            PlayerPrefs.SetString(nickNameKey, value);
-        }
+        get { return PlayerPrefs.GetString(nickNameKey); }
+        set { PlayerPrefs.SetString(nickNameKey, value); }
     }
 
-    // play Scene에서 최고 점수
-    public static int BestScore
+    public static int Level
     {
         get
         {
-            return PlayerPrefs.GetInt(bestScoreKey);
-        }
-        set
-        {
-            PlayerPrefs.SetInt(bestScoreKey, value);
-        }
-    }    
+            if (!PlayerPrefs.HasKey(levelKey))
+            {
+                PlayerPrefs.SetInt(levelKey, 1);
+            }
 
-    // BGM 볼륨
-    static float SfxVolume_default = 0.5f;
+            return PlayerPrefs.GetInt(levelKey);
+        }
+        set { PlayerPrefs.SetInt(levelKey, value); }
+    }
+
+    public static int Experience
+    {
+        get { return PlayerPrefs.GetInt(experienceKey); }
+        set { PlayerPrefs.SetInt(experienceKey, value); }
+    }
+
+    public static float HP
+    {
+        get { return PlayerPrefs.GetFloat(hpKey); }
+        set { PlayerPrefs.SetFloat(hpKey, value); }
+    }
+
+    public static int Strength
+    {
+        get { return PlayerPrefs.GetInt(strengthKey); }
+        set { PlayerPrefs.SetInt(strengthKey, value); }
+    }
+
+    public static int Agility
+    {
+        get { return PlayerPrefs.GetInt(agilityKey); }
+        set { PlayerPrefs.SetInt(agilityKey, value); }
+    }
+
+    public static int Health
+    {
+        get { return PlayerPrefs.GetInt(healthKey); }
+        set { PlayerPrefs.SetInt(healthKey, value); }
+    }
+
+    public static int Wisdom
+    {
+        get { return PlayerPrefs.GetInt(wisdomKey); }
+        set { PlayerPrefs.SetInt(wisdomKey, value); }
+    }
+
+    public static int Charm
+    {
+        get { return PlayerPrefs.GetInt(charmKey); }
+        set { PlayerPrefs.SetInt(charmKey, value); }
+    }
+
+    public static int Gold
+    {
+        get { return PlayerPrefs.GetInt(goldKey); }
+        set { PlayerPrefs.SetInt(goldKey, value); }
+    }
+
+    public static int Weapon
+    {
+        get { return PlayerPrefs.GetInt(weaponKey); }
+        set { PlayerPrefs.SetInt(weaponKey, value); }
+    }
+
+    public static int Armor
+    {
+        get { return PlayerPrefs.GetInt(armorKey); }
+        set { PlayerPrefs.SetInt(armorKey, value); }
+    }
+
+    public static int SmallHpPotion
+    {
+        get { return PlayerPrefs.GetInt(smallHpPotionKey); }
+        set { PlayerPrefs.SetInt(smallHpPotionKey, value); }
+    }
+
+    public static int LargeHpPotion
+    {
+        get { return PlayerPrefs.GetInt(largeHpPotionKey); }
+        set { PlayerPrefs.SetInt(largeHpPotionKey, value); }
+    }
+
     public static float SfxVolume
     {
-        get 
+        get
         {
-            // 이전 저장 데이터 없는 경우 기본 값 할당
             if (!PlayerPrefs.HasKey(sfxVolumeKey))
             {
-                PlayerPrefs.SetFloat(sfxVolumeKey, SfxVolume_default);
+                PlayerPrefs.SetFloat(sfxVolumeKey, 0.5f);
             }
 
             return PlayerPrefs.GetFloat(sfxVolumeKey);
         }
-        set
-        {
-            PlayerPrefs.SetFloat(sfxVolumeKey, value);
-        }
+        set { PlayerPrefs.SetFloat(sfxVolumeKey, value); }
     }
 
-    // SFX 볼륨
-    static float BgmVolume_default = 0.5f;
     public static float BgmVolume
     {
         get
         {
-            // 이전 저장 데이터 없는 경우 기본 값 할당
             if (!PlayerPrefs.HasKey(bgmVolumeKey))
             {
-                PlayerPrefs.SetFloat(bgmVolumeKey, BgmVolume_default);
+                PlayerPrefs.SetFloat(bgmVolumeKey, 0.5f);
             }
 
             return PlayerPrefs.GetFloat(bgmVolumeKey);
-
         }
-        set
-        {
-            PlayerPrefs.SetFloat(bgmVolumeKey, value);
-        }
+        set { PlayerPrefs.SetFloat(bgmVolumeKey, value); }
     }
-
-    // 모든 저장 데이터 삭제 메소드
     public static void ClearData()
     {
         PlayerPrefs.DeleteAll();
